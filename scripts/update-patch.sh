4849196c81b41f30a3b3144711a13dc7b1335c0c
Kot <1192090+koteq@users.noreply.github.com>
2025-06-08 14:21:03 +0000

    Update map-renderer patch

--- scripts/update-patch.sh
+++ scripts/update-patch.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-git -C ss14-sunrise diff > "$script_dir/../patches/map-renderer.patch"
+git -C ss14 diff > "$script_dir/../patches/map-renderer.patch"
